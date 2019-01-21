64ae18fee7fdded6874b23a795109e47c80fddd9
HimekaidouHatate <HimekaidouHatate@users.noreply.github.com>
2019-01-21 13:39:15 +0900

    temp

--- scripts/prefabs/polter.lua
+++ scripts/prefabs/polter.lua
@@ -1,76 +1,33 @@
 local MakePlayerCharacter = require "prefabs/player_common"
 
-
 local assets = {
     Asset("SCRIPT", "scripts/prefabs/player_common.lua"),
 }
 local prefabs = {}
 
--- Custom starting inventory
 local start_inv = {
 	"polterknife"
 }
 
--- When the character is revived from human
-local function onbecamehuman(inst)
-	-- Set speed when not a ghost (optional)
-	inst.components.locomotor:SetExternalSpeedMultiplier(inst, "polter_speed_mod", 1)
-end
-
-local function onbecameghost(inst)
-	-- Remove speed modifier when becoming a ghost
-   inst.components.locomotor:RemoveExternalSpeedMultiplier(inst, "polter_speed_mod")
-end
-
--- When loading or spawning the character
-local function onload(inst)
-    inst:ListenForEvent("ms_respawnedfromghost", onbecamehuman)
-    inst:ListenForEvent("ms_becameghost", onbecameghost)
-
-    if inst:HasTag("playerghost") then
-        onbecameghost(inst)
-    else
-        onbecamehuman(inst)
-    end
-end
-
-
--- This initializes for both the server and client. Tags can be added here.
 local common_postinit = function(inst) 
-	-- Minimap icon
 	inst.MiniMapEntity:SetIcon( "polter.tex" )
 end
 
--- This initializes for the server only. Components are added here.
 local master_postinit = function(inst)
-	-- choose which sounds this character will play
 	inst.soundsname = "willow"
 	inst.starting_inventory = start_inv
-	
-	-- Uncomment if "wathgrithr"(Wigfrid) or "webber" voice is used
-    --inst.talker_path_override = "dontstarve_DLC001/characters/"
-	
-	-- Stats	
+
 	inst.components.health:SetMaxHealth(300)
 	inst.components.hunger:SetMax(500)
+	inst.components.hunger:SetKillRate(10) -- 배고픔 0일때 체력이 1초에 10씩닳는다.
+	inst.components.hunger.hungerrate = 2 * TUNING.WILSON_HUNGER_RATE
 	inst.components.sanity:SetMax(250)
 	
-	-- Damage multiplier (optional)
     inst.components.combat.damagemultiplier = 2
-	
-	-- Hunger rate (optional)
-	inst.components.hunger.hungerrate = 2 * TUNING.WILSON_HUNGER_RATE
-	
-	inst.components.hunger:SetKillRate(10)
-	-- 배고픔 0일때 체력이 1초에 10씩닳는다.
-	
 	inst.components.combat.min_attack_period = 0.2
 	
 	inst.components.locomotor.walkspeed = (TUNING.WILSON_WALK_SPEED * 1.5)
 	inst.components.locomotor.runspeed = (TUNING.WILSON_RUN_SPEED * 1.6)
-	
-	inst.OnLoad = onload
-    inst.OnNewSpawn = onload
 	
 end
 
